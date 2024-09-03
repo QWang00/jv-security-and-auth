03f7ea7472345a4578a4f1082ecc789e139164e3
Liam O'Hara <liam.ohara@gmail.com>
2024-09-03 16:14:15 +0100

    Feat: add H2 access.

--- src/main/java/com/northcoders/security/security/SecurityConfig.java
+++ src/main/java/com/northcoders/security/security/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.web.servlet.function.RequestPredicates.headers;
 
 @EnableWebSecurity
 @Configuration
@@ -17,9 +19,12 @@ public class SecurityConfig {
         http.authorizeHttpRequests( auth -> {
             auth.requestMatchers("api/v1/protected/greeting").authenticated();
             auth.requestMatchers("api/v1/open/greeting").permitAll();
+            auth.requestMatchers(toH2Console()).permitAll();
+
 
         })
-                .oauth2Login(withDefaults());
+                .oauth2Login(withDefaults())
+                .headers(headers -> headers.frameOptions((frameOptions) -> frameOptions.disable()));
         return http.build();
     }
 }

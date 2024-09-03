a80e106e3e90511d8710bc205fe097f64e4a9d3e
Liam O'Hara <liam.ohara@gmail.com>
2024-09-03 15:11:28 +0100

    Feat: implement UsersRepository interface.

--- src/main/java/com/northcoders/security/repository/UsersRepository.java
+++ src/main/java/com/northcoders/security/repository/UsersRepository.java
@@ -1,8 +1,15 @@
 package com.northcoders.security.repository;
 
+import com.northcoders.security.model.Users;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UsersRepository extends CrudRepository (Users Long) {
+public interface UsersRepository extends CrudRepository <Users, Long> {
+
+    Optional<Users> findByGithubUsername(String githubUsername);
+
 }

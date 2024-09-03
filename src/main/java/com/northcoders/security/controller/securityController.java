cd35217be05490823dcfc5560f3ddbc3dd2087c3
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-03 14:38:37 +0100

    refactor: print username to greet in getAuthenticatedWelcome in securityController class

--- src/main/java/com/northcoders/security/controller/securityController.java
+++ src/main/java/com/northcoders/security/controller/securityController.java
@@ -1,5 +1,8 @@
 package com.northcoders.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,12 @@ public class securityController {
     }
 
     @GetMapping("/protected/greeting")
-    public static String getAuthenticatedWelcome(){
-        return "Welcome back, my friend!";
+    public static String getAuthenticatedWelcome(@AuthenticationPrincipal OAuth2User user){
+
+        return "Welcome back, my friend! " + user.getAttribute("login");
+
     }
+
+
 
 }
